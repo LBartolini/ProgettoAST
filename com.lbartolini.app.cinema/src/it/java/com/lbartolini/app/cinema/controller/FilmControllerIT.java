0c305a34c3bd36a561aa23c9b11f8b0af257256b
Lorenzo <l.bartolini02@gmail.com>
2025-06-20 15:40:55 +0200

    Setup pom with build-helper and failsafe

---
+++ com.lbartolini.app.cinema/src/it/java/com/lbartolini/app/cinema/controller/FilmControllerIT.java
@@ -0,0 +1,24 @@
+package com.lbartolini.app.cinema.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FilmControllerIT {
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
